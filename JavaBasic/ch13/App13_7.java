9c9c9f6d4b59e62287a48d57004765915f086ead
hejiangda <hejiangda@163.com>
2020-02-13 10:38:14 +0800

    省略括号和return

--- JavaBasic/ch13/App13_7.java
+++ JavaBasic/ch13/App13_7.java
@@ -6,7 +6,7 @@ interface IntFun
 public class App13_7
 {
     public static void main(String[] args) {
-        IntFun fun=(i)->{return 2*i;};
+        IntFun fun=i->2*i;
         double m=fun.dis(3);
         System.out.println(m);
     }
