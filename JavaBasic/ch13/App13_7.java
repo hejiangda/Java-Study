80d56df38b77618d07d7ba56e86e96bdc9d0eafb
hejiangda <hejiangda@163.com>
2020-02-13 10:28:28 +0800

    lambda表达式重写匿内部类

---
+++ JavaBasic/ch13/App13_7.java
@@ -0,0 +1,13 @@
+@FunctionalInterface
+interface IntFun
+{
+    double dis(int n);
+}
+public class App13_7
+{
+    public static void main(String[] args) {
+        IntFun fun=(i)->{return 2*i;};
+        double m=fun.dis(3);
+        System.out.println(m);
+    }
+}
